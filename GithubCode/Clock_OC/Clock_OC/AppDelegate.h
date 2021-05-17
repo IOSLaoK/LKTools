0dc70f754cc8a9819b18cf06bb0bc14fa42dbfaa
IOSLaoK <313812485@qq.com>
2021-05-17 16:19:23 +0800

    startCommit

---
+++ GithubCode/Clock_OC/Clock_OC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Clock_OC
+//
+//  Created by Bill on 2020/8/26.
+//  Copyright © 2020 mdkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GithubCode/Clock_OC/Clock_OC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Clock_OC
+//
+//  Created by Bill on 2020/8/26.
+//  Copyright © 2020 mdkj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
